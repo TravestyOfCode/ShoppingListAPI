5fe9de1802e2aab336ba6befd31b46c19a82af54
travis@travestyofcode.com <travis@travestyofcode.com>
2022-05-12 15:29:28 -0400

    Removes old DTO and creates new DTO.

---
+++ ShoppingListAPI/Services/UnitOfMeasure/UnitOfMeasureDTO.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+namespace ShoppingListAPI.Services.UnitOfMeasure
+{
+    public class UnitOfMeasureDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public static class UnitOfMeasureDTOMappingExtensions
+    {
+        public static IQueryable<UnitOfMeasureDTO> ProjectToDTO(this IQueryable<Data.UnitOfMeasure> query)
+        {
+            if (query == null)
+                return null;
+
+            return query.Select(p => new UnitOfMeasureDTO()
+            {
+                Id = p.Id,
+                Name = p.Name
+            });            
+        }
+
+        public static UnitOfMeasureDTO AsUnitOfMeasureDTO(this Data.UnitOfMeasure unitOfMeasure)
+        {
+            if (unitOfMeasure == null)
+                return null;
+
+            return new() { Id = unitOfMeasure.Id, Name = unitOfMeasure.Name };
+        }
+    }
+}
