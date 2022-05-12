cd59a45c32cca968d4f8d8e4c9d8f401385e0298
travis@travestyofcode.com <travis@travestyofcode.com>
2022-05-12 12:22:38 -0400

    Adds Try/Catch and Exception logging.

--- ShoppingListAPI/Controllers/AuthenticateController.cs
+++ ShoppingListAPI/Controllers/AuthenticateController.cs
@@ -3,15 +3,14 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using ShoppingListAPI.Data.Authentication;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace ShoppingListAPI.Controllers
@@ -24,68 +23,87 @@ namespace ShoppingListAPI.Controllers
 
         private readonly IConfiguration _configuration;
 
-        public AuthenticateController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+        private readonly ILogger<AuthenticateController> _logger;
+
+        public AuthenticateController(UserManager<IdentityUser> userManager, IConfiguration configuration, ILogger<AuthenticateController> logger)
         {
             _userManager = userManager;
             _configuration = configuration;
+            _logger = logger;
         }
 
         [HttpPost]
         [Route("login")]
         public async Task<IActionResult> Login([FromBody]Login login)
         {
-            var user = await _userManager.FindByNameAsync(login.UserName);
-
-            if(user != null && await _userManager.CheckPasswordAsync(user, login.Password))
+            try
             {
-                var userRoles = await _userManager.GetRolesAsync(user);
+                var user = await _userManager.FindByNameAsync(login.UserName);
 
-                var authClaims = new List<Claim>
+                if (user != null && await _userManager.CheckPasswordAsync(user, login.Password))
+                {
+                    var userRoles = await _userManager.GetRolesAsync(user);
+
+                    var authClaims = new List<Claim>
                 {
                     new Claim(ClaimTypes.Name, user.UserName),
                     new Claim(ClaimTypes.NameIdentifier, user.Id),
                     new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
                 };
 
-                foreach(var userRole in userRoles)
-                {
-                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                    foreach (var userRole in userRoles)
+                    {
+                        authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                    }
+
+                    var token = GetToken(authClaims);
+
+                    return Ok(new
+                    {
+                        token = new JwtSecurityTokenHandler().WriteToken(token),
+                        expiration = token.ValidTo
+                    });
                 }
 
-                var token = GetToken(authClaims);
-
-                return Ok(new
-                {
-                    token = new JwtSecurityTokenHandler().WriteToken(token),
-                    expiration = token.ValidTo
-                });
+                return Unauthorized();
             }
-
-            return Unauthorized();
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "Unexpected error trying to login: {login}", login);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpPost]
         [Route("register")]
         public async Task<IActionResult> Register([FromBody] Register register)
         {
-            var userExists = await _userManager.FindByNameAsync(register.UserName);
-            if (userExists != null)
-                return StatusCode(StatusCodes.Status500InternalServerError, new { Status = "Error", Message = "User already exists." });
-
-            IdentityUser user = new()
+            try
             {
-                Email = register.Email,
-                SecurityStamp = Guid.NewGuid().ToString(),
-                UserName = register.UserName
-            };
+                var userExists = await _userManager.FindByNameAsync(register.UserName);
+                if (userExists != null)
+                    return StatusCode(StatusCodes.Status500InternalServerError, new { Status = "Error", Message = "User already exists." });
 
-            var result = await _userManager.CreateAsync(user, register.Password);
-            if (!result.Succeeded)
-                return StatusCode(StatusCodes.Status500InternalServerError, new { Staus = "Error", Message = "User creation failed! Please check user details and try again." });
+                IdentityUser user = new()
+                {
+                    Email = register.Email,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    UserName = register.UserName
+                };
 
-            await _userManager.AddToRoleAsync(user, Roles.User);
+                var result = await _userManager.CreateAsync(user, register.Password);
+                if (!result.Succeeded)
+                    return StatusCode(StatusCodes.Status500InternalServerError, new { Staus = "Error", Message = "User creation failed! Please check user details and try again." });
 
-            return Ok(new { Status = "Success", Message = "User created successfully!" });
+                await _userManager.AddToRoleAsync(user, Roles.User);
+
+                return Ok(new { Status = "Success", Message = "User created successfully!" });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unexpected error trying to register: {register}", register);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpPost]
@@ -93,25 +111,33 @@ namespace ShoppingListAPI.Controllers
         [Authorize(Roles = Roles.Admin)]
         public async Task<IActionResult> RegisterAdmin([FromBody] Register register)
         {
-            var userExists = await _userManager.FindByNameAsync(register.UserName);
-            if (userExists != null)
-                return StatusCode(StatusCodes.Status500InternalServerError, new { Status = "Error", Message = "User already exists." });
-
-            IdentityUser user = new()
+            try
             {
-                Email = register.Email,
-                SecurityStamp = Guid.NewGuid().ToString(),
-                UserName = register.UserName
-            };
+                var userExists = await _userManager.FindByNameAsync(register.UserName);
+                if (userExists != null)
+                    return StatusCode(StatusCodes.Status500InternalServerError, new { Status = "Error", Message = "User already exists." });
 
-            var result = await _userManager.CreateAsync(user, register.Password);
-            if (!result.Succeeded)
-                return StatusCode(StatusCodes.Status500InternalServerError, new { Staus = "Error", Message = "User creation failed! Please check user details and try again." });
+                IdentityUser user = new()
+                {
+                    Email = register.Email,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    UserName = register.UserName
+                };
 
-            await _userManager.AddToRoleAsync(user, Roles.User);
-            await _userManager.AddToRoleAsync(user, Roles.Admin);
+                var result = await _userManager.CreateAsync(user, register.Password);
+                if (!result.Succeeded)
+                    return StatusCode(StatusCodes.Status500InternalServerError, new { Staus = "Error", Message = "User creation failed! Please check user details and try again." });
 
-            return Ok(new { Status = "Success", Message = "User created successfully!" });
+                await _userManager.AddToRoleAsync(user, Roles.User);
+                await _userManager.AddToRoleAsync(user, Roles.Admin);
+
+                return Ok(new { Status = "Success", Message = "User created successfully!" });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unexpected error trying to register-admin: {register}", register);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         private JwtSecurityToken GetToken(List<Claim> authClaims)
--- ShoppingListAPI/Controllers/UnitOfMeasureController.cs
+++ ShoppingListAPI/Controllers/UnitOfMeasureController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using ShoppingListAPI.Data;
 using ShoppingListAPI.Data.Authentication;
 using ShoppingListAPI.Models;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,28 +18,47 @@ namespace ShoppingListAPI.Controllers
     {
         private readonly ApplicationDbContext _dbContext;
 
-        public UnitOfMeasureController(ApplicationDbContext dbContext)
+        private readonly ILogger<UnitOfMeasureController> _logger;
+
+        public UnitOfMeasureController(ApplicationDbContext dbContext, ILogger<UnitOfMeasureController> logger)
         {
             _dbContext = dbContext;
+            _logger = logger;
         }
 
         [HttpGet]
         [Route("")]
         public async Task<IActionResult> Get(CancellationToken cancellationToken)
         {
-            return Ok(await _dbContext.UnitOfMeasures.ToListAsync(cancellationToken));
+            try
+            {
+                return Ok(await _dbContext.UnitOfMeasures.ToListAsync(cancellationToken));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unexpected error getting UnitOfMeasures.");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpGet]
         [Route("{id}")]
         public async Task<IActionResult> Get([FromRoute]int id, CancellationToken cancellationToken)
         {
-            var entity = await _dbContext.UnitOfMeasures.SingleOrDefaultAsync(p => p.Id.Equals(id), cancellationToken);
+            try
+            {
+                var entity = await _dbContext.UnitOfMeasures.SingleOrDefaultAsync(p => p.Id.Equals(id), cancellationToken);
 
-            if (entity == null)
-                return NotFound();
+                if (entity == null)
+                    return NotFound();
 
-            return Ok(entity);
+                return Ok(entity);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unexpected error getting UnitOfMeasure for id: {id}.", id);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpPost]
@@ -44,14 +66,22 @@ namespace ShoppingListAPI.Controllers
         [Authorize(Roles = Roles.Admin)]
         public async Task<IActionResult> Create([FromBody]UnitOfMeasureDTO uom, CancellationToken cancellationToken)
         {
-            if (!ModelState.IsValid)
-                return BadRequest();
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest();
 
-            var entity = _dbContext.UnitOfMeasures.Add(uom.AsUnitOfMeasure());
+                var entity = _dbContext.UnitOfMeasures.Add(uom.AsUnitOfMeasure());
 
-            await _dbContext.SaveChangesAsync(cancellationToken);
+                await _dbContext.SaveChangesAsync(cancellationToken);
 
-            return CreatedAtAction(nameof(Get), new { id = entity.Entity.Id }, entity.Entity);
+                return CreatedAtAction(nameof(Get), new { id = entity.Entity.Id }, entity.Entity);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unexpected error creating UnitOfMeasure: {uom}.", uom);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpPost]
@@ -59,19 +89,27 @@ namespace ShoppingListAPI.Controllers
         [Authorize(Roles = Roles.Admin)]
         public async Task<IActionResult> Edit([FromRoute] int id, [FromBody] UnitOfMeasureDTO uom, CancellationToken cancellationToken)
         {
-            if (!ModelState.IsValid)
-                return BadRequest();
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest();
 
-            var entity = await _dbContext.UnitOfMeasures.SingleOrDefaultAsync(p => p.Id.Equals(id), cancellationToken);
+                var entity = await _dbContext.UnitOfMeasures.SingleOrDefaultAsync(p => p.Id.Equals(id), cancellationToken);
 
-            if (entity == null)
-                return NotFound();
+                if (entity == null)
+                    return NotFound();
 
-            uom.MapTo(entity);
+                uom.MapTo(entity);
 
-            await _dbContext.SaveChangesAsync(cancellationToken);
+                await _dbContext.SaveChangesAsync(cancellationToken);
 
-            return Ok();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unexpected error editing UnitOfMeasure: {id}, {uom}.", id, uom);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpDelete]
@@ -79,16 +117,24 @@ namespace ShoppingListAPI.Controllers
         [Authorize(Roles = Roles.Admin)]
         public async Task<IActionResult> Delete([FromRoute] int id, CancellationToken cancellationToken)
         {
-            var entity = await _dbContext.UnitOfMeasures.SingleOrDefaultAsync(p => p.Id.Equals(id), cancellationToken);
+            try
+            {
+                var entity = await _dbContext.UnitOfMeasures.SingleOrDefaultAsync(p => p.Id.Equals(id), cancellationToken);
 
-            if (entity == null)
-                return NotFound();
+                if (entity == null)
+                    return NotFound();
 
-            _dbContext.UnitOfMeasures.Remove(entity);
+                _dbContext.UnitOfMeasures.Remove(entity);
 
-            await _dbContext.SaveChangesAsync(cancellationToken);
+                await _dbContext.SaveChangesAsync(cancellationToken);
 
-            return Ok();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unexpected error deleting UnitOfMeasure for id: {id}.", id);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }
